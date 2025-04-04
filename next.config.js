e90b6c507212b96c5155ce9373f3fd583c755164
srikanth-redbangle <srikanth@redbangle.com>
2025-04-04 19:31:22 +0530

    basepathe removed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: true,
-  basePath: basePath,
-  assetPrefix: basePath,
-  trailingSlash: true,
   // compiler: {
   //   removeConsole: false,
   // },
