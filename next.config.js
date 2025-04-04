8e084cfb5cdae30732a2d489573b6d95bad01011
srikanth-redbangle <srikanth@redbangle.com>
2025-04-04 12:40:11 +0530

    basepath asstspath added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/path2',
+  assetPrefix: '/path2',
   // compiler: {
   //   removeConsole: false,
   // },
