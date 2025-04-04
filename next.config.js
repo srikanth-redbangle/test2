61fb7d99f0ccdb9ce2bd0ebb32271dfa9f8e5633
srikanth-redbangle <srikanth@redbangle.com>
2025-04-04 09:53:38 +0530

    path changes reverted

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/path2',
-  assetPrefix: '/path2',
-  trailingSlash: true,
   // compiler: {
   //   removeConsole: false,
   // },
