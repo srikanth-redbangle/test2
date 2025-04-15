7d063af56378866023c7f9bdd7a70ba2b5a64eb2
srikanth-redbangle <srikanth@redbangle.com>
2025-04-15 09:30:04 +0530

    redirect added

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath:'/abc',
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/abc',
-  //       permanent: false, // set to true if you're sure
-  //     },
-  //   ];
-  // },
-  // compiler: {
-  //   removeConsole: false,
-  // },
+  //basePath:'/abc',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/abc',
+        permanent: false, // set to true if you're sure
+      },
+    ];
+  },
+  compiler: {
+    removeConsole: false,
+  },
 }
 
 module.exports = nextConfig
